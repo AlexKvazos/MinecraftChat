2327dcafe2b39279ccbfaebc459b24b7e7418afd
AlexKvazos <contact@alexkvazos.com>
2015-10-10 15:06:27 -0500

    Allow CORS

--- app/server/index.js
+++ app/server/index.js
@@ -22,7 +22,14 @@ let app     = express();
 let server  = createServer(app);
 let io      = socketio(server);
 
-io.set('origins', '*');
+// allow cors
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  next();
+});
 
 // setup socket.io-redis if connection variables are set
 if (process.env.REDIS_HOST && process.env.REDIS_PORT) {
